abbeb95a99c56d7a0703826f9e189c56a89720bb
AlinaZ45 <guest.relations@deltahotelsbymarriottantalyalara.com>
2026-09-24 18:05:14 +0300

    Refresh staff cache for MICROS layout

--- staff-sw.js
+++ staff-sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='sushi-club-staff-v3';
+const CACHE_NAME='sushi-club-staff-v4';
 const APP_SHELL=['/staff','/staff.html','/staff.js','/staff-manifest.webmanifest','/staff-icon.svg'];
 
 self.addEventListener('install',event=>{
