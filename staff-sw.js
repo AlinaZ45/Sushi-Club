c57fbcc6ed4b3722ee3000049917e1f00d6371ac
AlinaZ45 <guest.relations@deltahotelsbymarriottantalyalara.com>
2026-09-24 17:52:20 +0300

    Refresh staff cache for language support

--- staff-sw.js
+++ staff-sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='sushi-club-staff-v2';
+const CACHE_NAME='sushi-club-staff-v3';
 const APP_SHELL=['/staff','/staff.html','/staff.js','/staff-manifest.webmanifest','/staff-icon.svg'];
 
 self.addEventListener('install',event=>{
